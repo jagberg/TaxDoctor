d5b2732e0f46b6ee691897e33f37dd8b3b7dd2f0
jagberg <jagberg@61bbcbae-c3dd-483a-a886-1f8244e83f84>
2012-11-14 16:18:00 +0000

    Add Contact Us with email validation. Updated to .Net 4.5 so email valiation would work. After doing this the rounding of each section didnt work in IE but would in Chrome. I reverted back to 4.0 but this didnt help. Getting the latest version from Assembla also didnt work.

---
+++ trunk/Main/Codebase/TaxDoctor/TaxDoctor.Web/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaxDoctor.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ViewResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult ContactUs(ContactModel contact)
+        {
+            if (ModelState.IsValid)
+            {
+                EmailProcessor emailProcessor = new EmailProcessor();
+                emailProcessor.SendEmail(contact);
+
+                return View(new ContactModel());
+            }
+            else
+            {
+                return View(contact);
+            }
+        }
+    }
+}
---
+++ trunk/Main/Codebase/TaxDoctor/TaxDoctor.Web/Models/ContactModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaxDoctor.Web
+{
+    public enum WorkType
+    {
+        IndividualService,
+        BusinessService,
+        Training
+    }
+
+    public class ContactModel
+    {
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "* Please enter")]
+        public string Name { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "* Please enter")]
+        //[EmailAddress]
+        public string EmailAddress { get; set; }
+
+        public WorkType RequiredWork { get; set; }
+
+        [Required(ErrorMessage = "* Please enter")]
+        [DataType(DataType.MultilineText)]
+        public string Comment { get; set; }
+    }
+}
