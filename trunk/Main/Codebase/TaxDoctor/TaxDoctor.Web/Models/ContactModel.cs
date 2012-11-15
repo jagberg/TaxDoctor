20b0c20a2e01881030fed227a04b3150cc1634ab
jagberg <jagberg@61bbcbae-c3dd-483a-a886-1f8244e83f84>
2012-11-15 15:57:34 +0000

    Added Contact Form which follows RenderAction->Contact which then renders a partial view. This is required so that RedirectToAction would work. If you dont render a partial view then an error comes up saying you cant redirect a child action. Still needs to be done: If validation fails then display it in the Contact Us form, not the body.

--- trunk/Main/Codebase/TaxDoctor/TaxDoctor.Web/Models/ContactModel.cs
+++ trunk/Main/Codebase/TaxDoctor/TaxDoctor.Web/Models/ContactModel.cs
@@ -22,8 +22,8 @@ namespace TaxDoctor.Web
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
-        [Required(ErrorMessage = "* Please enter")]
-        //[EmailAddress]
+        [EmailAddress(ErrorMessage="* Invalid email")]
+        //[Required(ErrorMessage = "* Please enter")]
         public string EmailAddress { get; set; }
 
         public WorkType RequiredWork { get; set; }
@@ -31,5 +31,14 @@ namespace TaxDoctor.Web
         [Required(ErrorMessage = "* Please enter")]
         [DataType(DataType.MultilineText)]
         public string Comment { get; set; }
+
+        public bool IsValid()
+        {
+            if (string.IsNullOrWhiteSpace(this.EmailAddress) && string.IsNullOrWhiteSpace(this.PhoneNumber))
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
