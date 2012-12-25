218a353d3a75aec960f7fb61a424822e6e6b33da
jagberg <6295138+jagberg@users.noreply.github.com>
2012-12-25 22:41:35 +0000

    Added Publish file for WinHost. Changed reference files to copy to local bin as its required for WinHost. Added Full trust to config. Added payroll image to project. Replaced Log4Net with FM Log4Net for 4.5 because the following error came up on WinHost: Inheritance security rules violated while overriding member: 'log4net.Util.ReadOnlyPropertiesDictionary.GetObjectData(System.Runtime.Serialization.SerializationInfo, System.Runtime.Serialization.StreamingContext)'. Security accessibility of the overriding method must match the security accessibility of the method being overriden.

--- Main/Codebase/TaxDoctor/TaxDoctor.Web/Logger.cs
+++ Main/Codebase/TaxDoctor/TaxDoctor.Web/Logger.cs
@@ -1,4 +1,4 @@
-﻿using log4net;
+﻿using FM.Log4Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +19,7 @@ namespace TaxDoctor.Web
 
     public class Logger : ILogger
     {
-        private static readonly ILog log = LogManager.GetLogger("TaxDoctorLogger");
+        private static readonly FM.Log4Net.LogProvider log = new LogProvider();
 
         public Logger()
         {
@@ -27,7 +27,7 @@ namespace TaxDoctor.Web
 
         internal static void Configure()
         {
-            log4net.Config.XmlConfigurator.Configure();
+            //log4net.Config.XmlConfigurator.Configure();
         }
 
         public void Log(string message, ErrorType errType)
