a686434233e957135c3fc0f9c86df73e3f52651b
Justin Goldberg <jagberg@gmail.com>
2015-02-02 09:31:44 +0000

    Imported from SVN by Bitbucket

---
+++ Main/Codebase/TaxDoctor/TaxDoctor.Web/Domain/EmailProcessor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaxDoctor.Web
+{
+    public class EmailProcessor
+    {
+        /// <summary>
+        /// Send email through Smtp client.
+        /// </summary>
+        /// <param name="contact"></param>
+        public void SendEmail(ContactModel contact)
+        {
+            var client = new SmtpClient(Config.EmailSmtp, Config.EmailSmtpPort)
+            {
+                Credentials = new NetworkCredential(Config.EmailUsername, Config.EmailPassword),
+                EnableSsl = true
+            };
+            client.Send(Config.EmailFrom, Config.EmailTo, Config.EmailSubject, GetContactEmailBody(contact));
+        }
+
+        /// <summary>
+        /// Format email body.
+        /// </summary>
+        /// <param name="contact"></param>
+        /// <returns></returns>
+        private string GetContactEmailBody(ContactModel contact)
+        {
+            StringBuilder body = new StringBuilder();
+
+            body.AppendLine(string.Format("Name: {0}", contact.Name));
+            body.AppendLine(string.Format("Email: {0}", contact.EmailAddress));
+            body.AppendLine(string.Format("Phone Number: {0}", contact.PhoneNumber));
+            //body.Append(string.Format("Required Work: {0}", contact.RequiredWork));
+            body.Append(string.Format("Comment: {0}", contact.Comment));
+
+            return body.ToString();
+        }
+    }
+}
---
+++ Main/Codebase/TaxDoctor/TaxDoctor.Web/Models/ContactModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaxDoctor.Web
+{
+    public enum WorkType
+    {
+        IndividualService,
+        BusinessService,
+        Training
+    }
+
+    public class ContactModel
+    {
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "* Please enter")]
+        public string Name { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress(ErrorMessage="* Invalid email")]
+        //[Required(ErrorMessage = "* Please enter")]
+        public string EmailAddress { get; set; }
+
+        public WorkType RequiredWork { get; set; }
+
+        [Required(ErrorMessage = "* Please enter")]
+        [DataType(DataType.MultilineText)]
+        public string Comment { get; set; }
+
+        public bool IsValid()
+        {
+            if (string.IsNullOrWhiteSpace(this.EmailAddress) && string.IsNullOrWhiteSpace(this.PhoneNumber))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
