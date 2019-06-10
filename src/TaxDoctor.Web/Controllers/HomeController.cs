f31b4579df0eae0a0e3909cc2b6e0dce76fb8171
Justin Goldberg <jagberg@gmail.com>
2019-06-10 21:03:01 +1000

    feat: SEO Enhancements
    Enable gzip compression
    Update titles to less than 60 char
    Add sitemap.xml which was recommended by a SEO analyzer. Not sure difference between sitemapspal.xml and this but why not
    Add alt text to images

--- src/TaxDoctor.Web/Controllers/HomeController.cs
+++ src/TaxDoctor.Web/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace TaxDoctor.Web.Controllers
 {
     public class HomeController : BaseController
     {
-        [PageTitle("Affordable accounting, bookkeeping, payroll and tax returns | Accountant in Johannesburg area")]
+        [PageTitle("Affordable accounting, bookkeeping, payroll and tax returns")]
         [MetaDescription("We offer an affordable service for all your tax concerns and including accounting, bookkeeping, training and personal tax returns.")]
         [MetaKeywords("tax, bookkeeping, bookkeeper, payroll, paye, irp5, irp6, it12, it14, emp201, emp501, vat201, cashflow, financial statements, company formations, annual returns, cipc, cipro, sars, sars easyfile, tax sumbission, workmens compensation, audits, quickbooks, pastel payroll, excel, sdl, uif, ck, tax clearance certificates, accounting officer")]
         public ActionResult Index()
@@ -18,7 +18,7 @@ namespace TaxDoctor.Web.Controllers
             return View();
         }
 
-        [PageTitle("About us - Affordable accounting, bookkeeping, payroll and tax returns in Johannesburg area")]
+        [PageTitle("About us - Affordable accounting, bookkeeping, payroll and tax returns in Jhb area")]
         [MetaDescription("We offer an affordable service for all your tax concerns including accounting, bookkeeping, training and personal tax returns.")]
         [MetaKeywords("about tax doctor, accountant, tax services, bookkeeping, bookkeeper, payroll, training, bookkeeping trainning, accounting training, company registration, company formation, sars, sars easyfile, cipro")]
         public ActionResult About()
